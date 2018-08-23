815f0813c2be0b1e50dbcac19647b6d960a6ef8b
Colt Street <Colt.Street@dataventures.com>
2018-08-23 15:59:25 -0400

    fix build errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAnqyczL2mutmqX5o65auznuONdQiQAXcA",
+    authDomain: "reviews-7780d.firebaseapp.com",
+    databaseURL: "https://reviews-7780d.firebaseio.com",
+    projectId: "reviews-7780d",
+    storageBucket: "reviews-7780d.appspot.com",
+    messagingSenderId: "759943165101"
+  }
 };
